3208892c97356e8f6b584fcec13330daf7bbcf84
Hyelin Kim <rlafls9596@gmail.com>
2021-03-23 14:56:24 +0900

    Update MemberDAO.java

--- Spring_3_1/src/main/java/com/iu/s3/member/MemberDAO.java
+++ Spring_3_1/src/main/java/com/iu/s3/member/MemberDAO.java
@@ -2,9 +2,12 @@ package com.iu.s3.member;
 
 public class MemberDAO {
 	
+	public void menberSelect()throws Exception{
+		//조회 아무거나
+	}
+	
 	public void memberUpdate()throws Exception {
 		//id룰 제외하고 나머지 수정
-		
 		
 	}
 	
